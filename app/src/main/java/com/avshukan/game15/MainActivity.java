eb6f4480c58078a727110bc434109921f5c33a39
Alexander Shukan <avshukan@mail.ru>
2016-12-06 05:35:03 +1100

    Try again

--- app/src/main/java/com/avshukan/game15/MainActivity.java
+++ app/src/main/java/com/avshukan/game15/MainActivity.java
@@ -1,13 +1,115 @@
 package com.avshukan.game15;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+//    private B[] board;
+
+    private TextView tt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        tt = (TextView) findViewById(R.id.textHelloWorld);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+
+//        board = new B[16];
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+//    class B {
+//        private String originN;
+//        private String currentN;
+//        private boolean moveable;
+//
+//        public void setMoveable(boolean moveable) {
+//            this.moveable = moveable;
+//        }
+//
+//        public String getCurrentN() {
+//            return currentN;
+//        }
+//
+//        public String getOriginN() {
+//            return originN;
+//        }
+//
+//        public void setCurrentN(String currentN) {
+//            this.currentN = currentN;
+//        }
+//
+//        public void setOriginN(String originN) {
+//            this.originN = originN;
+//        }
+//    }
+
+    public void onButtonNClick(View view)
+    {
+        // выводим сообщение
+        //showAlertDialog();
+        int id = view.getId();
+//        TextView t = (TextView) findViewById(R.id.textView);
+        tt.setText(Integer.toString(id));
+//        tt.setText("!!!");
+//        Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show();
     }
 }
